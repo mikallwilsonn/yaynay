a9db7fe8952005212ab03471b8713a01ccc77c76
Michael Wilson <info@michaelwilsion.io>
2019-01-05 19:58:52 -0500

    Displaying credit quantity

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -16,7 +16,8 @@ class Header extends Component {
             default: 
                 return [
                     <li key="1"><Payments /></li>,
-                    <li key="2"><a href="/api/logout">Logout</a></li>
+                    <li key="2" style={{ margin: '0 10px' }}>Credits: {this.props.auth.credits}</li>,
+                    <li key="3"><a href="/api/logout">Logout</a></li>
                 ];
         }
     }
@@ -28,7 +29,8 @@ class Header extends Component {
 
                     <Link 
                         to={ this.props.auth ? '/surveys' : '/' } 
-                        className="brand-logo"
+                        className="brand-logo" 
+                        style={{ marginLeft: '10px' }}
                     >Emaily</Link>
 
                     <ul id="nav-mobile" className="right">

606e65373919615d73bbcb63db06ee07569c80b5
Michael Wilson <info@michaelwilsion.io>
2019-01-05 19:31:36 -0500

    Finalizing a charge and adding credits to a user

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,10 +1,22 @@
 const keys = require( '../config/keys' );
 const stripe = require( 'stripe' )( keys.stripeSecretKey );
+const mongoose = require( 'mongoose' );
+const User = mongoose.model( 'users' );
 
 module.exports = app => {
 
-    app.post( '/api/stripe', ( req, res ) => {
+    app.post( '/api/stripe', async ( req, res ) => {
+        const charge = await stripe.charges.create({
+            amount: 500,
+            currency: 'usd',
+            description: 'Purchase of 5 credits for account',
+            source: req.body.id
+        });
 
+        req.user.credits += 5;
+        const user = await req.user.save();
+
+        res.send( user );
     });
 
 }

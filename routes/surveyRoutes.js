709bcc6cf2d13a34123e6ac6547dd7d763103c92
Michael Wilson <info@michaelwilsion.io>
2019-01-17 17:35:05 -0500

    Created a middleware for checking a users credit count

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,9 +1,10 @@
 const requireLogin = require( '../middlewares/requireLogin' );
+const requireCredits = require( '../middlewares/requireLogin' );
 
 module.exports = app => {
 
-    app.post( '/api/surveys', requireLogin , ( req, res ) => {
-        
+    app.post( '/api/surveys', requireLogin, requireCredits, ( req, res ) => {
+
     });
 
 };

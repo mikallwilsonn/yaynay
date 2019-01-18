5ea140bf8f5600907b2d740b54a0692db1f8f621
Michael Wilson <info@michaelwilsion.io>
2019-01-18 15:02:22 -0500

    Redux form setup

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SurveyNew extends Component {
+    render() {
+        return (
+            <div>
+                <p>New Survey Form</p>
+            </div>
+        );
+    }
+}
+
+export default SurveyNew;

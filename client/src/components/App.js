b45a21eef8a52b1bc8313e834c325883c1e3a573
Michael Wilson <info@michaelwilsion.io>
2019-01-18 21:47:47 -0500

    Form validation

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -20,11 +20,13 @@ class App extends Component {
         return (
             <div className="container">
                 <BrowserRouter>
-                    <div className="container">
+                    <div>
                         <Header />
-                        <Route exact path="/" component={Landing} />
-                        <Route exact path="/surveys" component={Dashboard} />
-                        <Route exact path="/surveys/new" component={SurveyNew} />
+                        <div className="container">
+                            <Route exact path="/" component={Landing} />
+                            <Route exact path="/surveys" component={Dashboard} />
+                            <Route exact path="/surveys/new" component={SurveyNew} />
+                        </div>
                     </div>
                 </BrowserRouter>
             </div>

aa2bbc1dd63bf68ae803d980cc6b33b4dafeb3e4
Michael Wilson <info@michaelwilsion.io>
2019-01-19 16:12:09 -0500

    Fixing property names

--- client/src/components/surveys/formFields.js
+++ client/src/components/surveys/formFields.js
@@ -5,6 +5,6 @@ export default [
         validationError: 'You must provide a subject for your survey.' },
     { label: 'Email Body', name: 'body', 
         validationError: 'Your survey needs content to show the recipient.' },
-    { label: 'Recipient List', name: 'emails', 
+    { label: 'Recipient List', name: 'recipients', 
         validationError: 'You must provide emails of those you would like to send your survey to.' }
 ];

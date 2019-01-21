1b5ff2ad1714b6618f6fa487c9e947d2ee363977
Michael Wilson <info@michaelwilsion.io>
2019-01-21 12:22:19 -0500

    Parsing the route

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require( 'lodash' );
+const { Path } = require( 'path-parser' );
+const { URL } = require( 'url' );
 const mongoose = require( 'mongoose' );
 const requireLogin = require( '../middlewares/requireLogin' );
 const requireCredits = require( '../middlewares/requireLogin' );
@@ -44,8 +47,11 @@ module.exports = app => {
 
 
     app.post( '/api/surveys/webhooks', ( req, res ) => {
-        console.log( req.body );
-        res.send({});
+        const events = _.map( req.body, ( event ) => {
+            const pathname = new URL( event.url ).pathname;
+            const p = new Path( '/api/surveys/:survey_id/:choice' );
+            console.log ( p.test( pathname ));
+        });
     });
 
 };

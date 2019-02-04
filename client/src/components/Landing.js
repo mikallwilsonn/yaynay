620d4f1ed3bb66a018856c01333303eb0c3715ca
Michael Wilson <info@michaelwilsion.io>
2019-02-04 18:05:08 -0500

    Changed app name everywhere

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -5,7 +5,7 @@ const Landing = () => {
         <div id="landing" style={{ textAlign: 'center'}}>
             <div>
                 <h1>
-                    Emaily
+                    YayNay
                 </h1>
                 <h4>Collect Amazing Feedback</h4>
                 <p>Quickly get <strong>yes</strong> or <strong>no</strong> answers to questsions in just a few simple steps.</p>
